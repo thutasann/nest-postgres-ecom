34aaa122da410167d6b3bba8e500307dcef81553
thutasann <66902308+thutasann@users.noreply.github.com>
2023-08-18 16:48:53 +0630

    feat: product create

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'Title cannot be blank' })
+  @IsString()
+  title: string;
+
+  @IsNotEmpty({ message: 'description cannot be empty' })
+  @IsString()
+  description: string;
+
+  @IsNotEmpty({ message: 'price should not be empty' })
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'price should be number & max decimal precission 2' },
+  )
+  @IsPositive({ message: 'price should be positive number' })
+  price: number;
+
+  @IsNotEmpty({ message: 'stock should not be empty.' })
+  @IsNumber(
+    {},
+    { message: 'stock should be number & max decimal precission 2' },
+  )
+  @Min(0, { message: 'Price cannot be negative' })
+  stock: number;
+
+  @IsNotEmpty({ message: 'images should not be empty' })
+  @IsArray({ message: 'Images should be in array format.' })
+  images: string[];
+
+  @IsNotEmpty({ message: 'category should not be empty' })
+  @IsNumber({}, { message: 'category id should be number' })
+  categoryId: number;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoriesService } from 'src/categories/categories.service';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductEntity } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRespository: Repository<ProductEntity>,
+    private readonly categoryService: CategoriesService,
+  ) {}
+
+  async create(
+    createProductDto: CreateProductDto,
+    currentUser: UserEntity,
+  ): Promise<ProductEntity> {
+    const category = await this.categoryService.findOne(
+      +createProductDto.categoryId,
+    );
+    const product = this.productRespository.create(createProductDto);
+    product.category = category;
+    product.addedBy = currentUser;
+    return await this.productRespository.save(product);
   }
 
   findAll() {
@@ -17,7 +37,7 @@ export class ProductsService {
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return `This action updates a #${id} product ${updateProductDto}`;
   }
 
   remove(id: number) {
